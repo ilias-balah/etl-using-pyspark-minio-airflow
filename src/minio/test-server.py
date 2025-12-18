866198c5ad06ed2f2d155ba0ecbb7b96e5ecb570
Ilias Balah <balah.ilias@gmail.com>
2025-12-18 18:25:50 -0500

    Test reading data, transforming it, and write it back to MinIO buckets

--- src/minio/test-server.py
+++ src/minio/test-server.py
@@ -1,9 +1,3 @@
-### Configure PySpark to use the created buckets
-
-See the script `src/minio/test-server.py`.
-
-```py
-
 import os
 import dotenv
 
@@ -17,9 +11,24 @@ def _set_spark_environment():
     Sets the SPARK_HOME environment variable based on the installed
     PySpark version.
     """
-    # Environment variable to specify the Spark home directory
-    # This is necessary for PySpark to find the required JAR files
-    ...
+    try:
+        # Read the spark version is installed
+        from pyspark import __version__ as spark_version
+
+        # Set the SPARK_HOME based on the spark version
+        os.environ['SPARK_HOME'] = 'c:/spark/spark-{}'.format(spark_version)
+        os.environ['HADOOP_HOME'] = 'c:/spark/hadoop'
+
+        # Set the hadoop aws package version based on spark version
+        os.environ['HADOOP_AWS_VERSION'] = '3.4.1' if spark_version.startswith('4.0') else '3.3.6'
+
+        # Log the settings
+        print("Using spark from {};".format(os.environ['SPARK_HOME']))
+        print("Using hadoop from {};".format(os.environ['HADOOP_HOME']))
+        print("Using hadoop aws package {};".format(os.environ['HADOOP_AWS_VERSION']))
+
+    except ImportError as e:
+        raise ImportError("PySpark is not installed. Please install it to proceed.") from e
     
 
 def _create_spark_session():
@@ -54,6 +63,14 @@ def _create_spark_session():
     # NOTE: Spark downloads these JARs at runtime and stores them in its temp directory.
     # On Windows, Spark may fail to delete these cached JARs during shutdown due to
     # file locks held by the JVM. This can produce "Failed to delete" warnings.
+    # To prevent this, especially during development and testing, and avoid re-downloading
+    # the JARs each time, we can use a workaround by manually moving the initially
+    # downloaded JARs from the Spark temp directory into a permanent Spark installation
+    # directory : spark-4.0.0\jars
+    # Required JARs to move are,
+    #   - org.apache.hadoop_hadoop-aws-3.4.1.jar
+    #   - org.wildfly.openssl_wildfly-openssl-1.1.3.Final.jar
+    #   - software.amazon.awssdk_bundle-2.24.6.jar
     if required_packages:
         spark_builder.config("spark.jars.packages", ",".join(required_packages))
 
@@ -72,7 +89,35 @@ def transform_data(data: DataFrame) -> DataFrame:
     Dummy transformation function to simulate data processing for 
     the customer data before writing it back to MinIO.
     """
-    # Transformation logic added here ...
+    # 1. Clean names by lowercasing and trimming all values
+    @functions.udf(returnType=types.StringType())
+    def lowercase_and_trim(value: str):
+        return value.strip().lower()
+    
+    data = \
+        data \
+            .withColumn('First Name', lowercase_and_trim('First Name')) \
+            .withColumn('Last Name', lowercase_and_trim('Last Name')) \
+            .withColumn('Company', lowercase_and_trim('Company')) \
+            .withColumn('Email', lowercase_and_trim('Email')) \
+    
+    # 2. Clean phone numbers by triming it with the xNNN.
+    @functions.udf(returnType=types.StringType())
+    def trim_xnnn(value: str):
+        value = value.split('x')[0]
+        value = value.lstrip('0')
+        return value
+    
+    data = \
+        data \
+            .withColumn('Phone 1', trim_xnnn('Phone 1')) \
+            .withColumn('Phone 2', trim_xnnn('Phone 2')) \
+
+    # 3. Add a new column with the full name
+    data = \
+        data \
+            .withColumn('Full Name', functions.concat_ws(' ', 'First Name', 'Last Name')) \
+
     return data
 
 
@@ -105,5 +150,3 @@ if __name__ == "__main__":
 
     # Run the ETL test with MinIO
     main()
-
-```
